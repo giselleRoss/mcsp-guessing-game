3975e48ab75f58b43f6d7230d4931b4920394e2f
GISELLE ROSS <GISELLEROSSTECH@GMAIL.COM>
2023-12-08 14:30:40 -0500

    feature 2 complete

--- app.js
+++ app.js
@@ -1,38 +1,27 @@
-console.log("app.js started");
+//create a random number called "target"
 
-const minGuess = 1;
-const maxGuess = 10;
-// create a random number called "target"
-//let target = getRandomIntInclusive(minGuess, maxGuess)
-const num = parseInt(Math.random() * 10);
+const target = parseInt(Math.random() * 10);
 
-while(true){
-    const result = guessOnce();
-    alert(result)
-    if(result === false) {
-        break;
-    }
-}
-
-function guessOnce() {
-    let input = prompt(`Guess a number between ${minGuess} and ${maxGuess}.`)
-    console.log(`The user guessed ${input}`)
+function guessUntilCorrect() {
+  let input = prompt(`Guess a number between 1 and 10.`)
+  console.log(`The user guessed ${input}`)
 // compare user to target
-    let inputNum = parseInt(input);
-    if(inputNum < target){
-        alert("Higher")
-
-    }else if(inputNum > target) {
-        alert("Lower")
-    }else {
-        alert("Correct")
-    }
+  let inputNum = parseInt(input);
+  if(inputNum < target){
+    alert("Higher. Try again!")
+  }else if(inputNum > target) {
+      alert("Lower. Try again!")
+  }else {
+      alert("Correct");
+  }
 }
 // use prompt to get user input 
 console.log(`The number was ${target}`)
-
-// inform user if guess is >, < or = to target
-
-// function getRandomIntInclusive(min, max) {
-//     return Math.floor(Math.random() * (max - min + 1) + min);
-// }
+while(true){
+  if(guessUntilCorrect()) {
+    break;
+  }
+  else{
+    continue;
+  }
+}
