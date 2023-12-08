617c6f24d9ab553a304af6d510106b54cbc9adbf
GISELLE ROSS <GISELLEROSSTECH@GMAIL.COM>
2023-12-08 17:36:34 -0500

    feature 6 complete

--- app.js
+++ app.js
@@ -1,27 +1,51 @@
-//create a random number called "target"
+const randomNumber = Math.floor(Math.random() * 10) + 1;
 
-const target = parseInt(Math.random() * 10);
-
-function guessUntilCorrect() {
-  let input = prompt(`Guess a number between 1 and 10.`)
-  console.log(`The user guessed ${input}`)
-// compare user to target
-  let inputNum = parseInt(input);
-  if(inputNum < target){
-    alert("Higher. Try again!")
-  }else if(inputNum > target) {
-      alert("Lower. Try again!")
-  }else {
-      alert("Correct");
+function parseInput(string){
+  if(string === ''){
+    return NaN;
+  } else if(string === null){
+    return null;
+  } else {
+    return Number(string);
   }
 }
-// use prompt to get user input 
-console.log(`The number was ${target}`)
-while(true){
-  if(guessUntilCorrect()) {
-    break;
+
+function promptForGuess(message){
+  let guess = prompt(message);
+  let guessNum = parseInput(guess);
+  while( !Number.isInteger(guessNum) || guessNum < 1 || guessNum > 10){
+    guessNum = parseInput(prompt("Please enter a valid number"));
   }
-  else{
-    continue;
+  return guessNum;
+}
+
+function guessingGame(){
+  const numTarget = Math.floor(Math.random() * 10) + 1;
+  let name = prompt("What is your name?");
+  let guess = promptForGuess("Guess a number between 1 and 10");
+
+  console.log(`User guessed ${guess}`);
+  console.log("The number was", numTarget);
+  let numGuesses = [guess];
+  while(guess !== numTarget){
+    if(guess < numTarget){
+      guess = promptForGuess(`Guess higher, ${name}`);
+    } else {
+      guess = promptForGuess(`Guess lower ${name}`);
+    }
+   numGuesses.push(guess);
+  }
+  alert(`Congratulations ${name}! It took you ${numGuesses.length} guesses. You guessed ${numGuesses} .`);
+  let playAgain = tryAgain("Would you like to play again? (y/n)");
+}
+
+function tryAgain(play){
+  let tryAgain = prompt("Would you like to play again? (y/n)");
+  if(tryAgain === "y" || tryAgain === "yes"){
+    guessingGame();
+  } else {
+    alert("Thanks for playing!");
   }
 }
+
+guessingGame();
